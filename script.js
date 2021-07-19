405079d422486cd18fae4df2d8080aa74c10c86d
Rolle Kekki <r2101k@protonmail.com>
2021-07-19 19:23:48 +0300

    added a new layout for the game

--- script.js
+++
@@ -1,8 +0,0 @@
-const para = document.querySelector("p");
-
-para.addEventListener("click", updateName);
-
-function updateName() {
-    let name = prompt("Enter player name");
-    para.textContent = "Player 1: " + name;
-}
